6800ed60687a587bd93ee39e7f2fee5ce38dff26
x2107 <89317804+x2107@users.noreply.github.com>
2021-11-06 12:50:00 -0400

    fireorm setup

--- backend/index.js
+++ backend/index.js
@@ -17,3 +17,16 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+import * as admin from 'firebase-admin';
+import * as fireorm from 'fireorm';
+
+const serviceAccount = require('../firestore.creds.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`,
+});
+
+const firestore = admin.firestore();
+fireorm.initialize(firestore);
