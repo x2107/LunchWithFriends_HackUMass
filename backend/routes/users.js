0ff419c03655b658d59ce9e5bf07efce0f5b3f23
x2107 <89317804+x2107@users.noreply.github.com>
2021-11-06 15:59:51 -0400

    user crud

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,32 +1,80 @@
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, } from "firebase/database";
 
-function CreateUser(userId, name) {
+function CreateUser(name) {
   const db = getDatabase();
-  set(ref(db, 'users/' + userId), {
-    username: name
+  const res = await db.collection('users').add({
+    username: name,
+    friends: [],
+    events: [],
+    groups: []
   });
+  
+  console.log('Added document with ID: ', res.id);
 }
 
-function AddUserFriends(userId, friendUserId) {
+function AddUserFriends(userId, friendId) {
     const db = getDatabase();
-    set(ref(db, 'users/' + userId), {
-      friends: friendUserId
-    });
-
+    const userRef = db.collection('users').doc(userId);
+    const unionRes = await userRef.update({
+        friends: FieldValue.arrayUnion(friendId)
+    })
+    console.log('Friend Added: ', friendId);
 }
 
-function AddUserEvents(userId, EventId) {
+function RemoveUserFriends(userId, friendUserId) {
     const db = getDatabase();
-    set(ref(db, 'users/' + userId), {
-      friends: EventId
-    });
-
+    const userRef = db.collection('users').doc(userId);
+    const unionRes = await userRef.update({
+        friends: FieldValue.arrayRemove(friendId)
+    })
+    console.log('Friend Removed: ', friendId);
 }
 
-function AddUserEvents(userId, groupId) {
+function GetFriends(userId) {
     const db = getDatabase();
-    set(ref(db, 'users/' + userId), {
-      groups: GroupId
-    });
+    get(child(dbRef, `users/${userId}/friendUserId`)).then((snapshot) => {
+        if (snapshot.exists()) {
+          return(snapshot.val());
+        } else {
+          console.log("No data available");
+        }
+      }).catch((error) => {
+        console.error(error);
+      });
+}
 
+function AddUserEvent(userId, eventId) {
+    const db = getDatabase();
+    const userRef = db.collection('users').doc(userId);
+    const unionRes = await userRef.update({
+        events: FieldValue.arrayUnion(eventId)
+    })
+    console.log('Event Added: ', eventId);
+}
+
+function RemoveUserEvent(userId, eventId) {
+    const db = getDatabase();
+    const userRef = db.collection('users').doc(userId);
+    const unionRes = await userRef.update({
+        events: FieldValue.arrayRemove(eventId)
+    })
+    console.log('Event Removed: ', eventId);
+}
+
+function AddUserGroup(userId, groupId) {
+    const db = getDatabase();
+    const userRef = db.collection('users').doc(userId);
+    const unionRes = await userRef.update({
+        groups: FieldValue.arrayUnion(groupId)
+    })
+    console.log('Event Added: ', groupId);
+}
+
+function RemoveUserGroup(userId, groupId) {
+    const db = getDatabase();
+    const userRef = db.collection('users').doc(userId);
+    const unionRes = await userRef.update({
+        groups: FieldValue.arrayRemove(groupId)
+    })
+    console.log('Event Removed: ', groupId);
 }
